034f30e2085ba642c780a7d0c83ae7642ed2df23
Brent Kie <brent.a.kie@gmail.com>
2018-05-06 17:39:50 -0600

    Added state variables with doc blocks to class.

--- classes/BeerStyle.php
+++ classes/BeerStyle.php
@@ -5,3 +5,18 @@ require_once("autoload.php");
 require_once(dirname(__DIR__, 2) . "/classes/autoload.php");
 
 use Ramsey\Uuid\Uuid;
+
+class beerstyle implements \JsonSerializable {
+	use ValidateUuid;
+	/**
+	 * Beer id for the BeerStyle
+	 * @var Uuid|string $beerStyleBeerId
+	 */
+	private $beerStyleBeerId;
+	/**
+	 * Style id for the BeerStyle
+	 * @var Uuid|tinyint $beerStyleStyleId
+	 */
+	private $beerStyleStyleId;
+
+}
