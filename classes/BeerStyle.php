170b4d34be44d617dcd044165ff61409b9354654
Brent Kie <brent.a.kie@gmail.com>
2018-05-07 17:23:06 -0600

    Added INSERT PDO method for BeerStyle class.

--- classes/BeerStyle.php
+++ classes/BeerStyle.php
@@ -90,5 +90,25 @@ class beerstyle implements \JsonSerializable {
 		}
 		$this->beerStyleStyleId = $uuid;
 	}
+
+	/**
+	 *inserts beer style into mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function insert(\PDO $pdo): void {
+
+		//create query template
+		$query = "INSERT INTO beerstyle (beerStyleBeerId, beerStyleStyleId) VALUES (:beerStyleBeerId, :beerStyleStyleId)";
+		$statement = $pdo->prepare($query);
+
+		//bind the member variables to the place holders in the template
+		$parameters = ["beerStyleBeerId" => $this->beerStyleBeerId->getBytes(), "beerStyleStyleType" => $this->beerStyleStyleId->getBytes()];
+		$statement->execute($parameters);
+	}
+
 }
+
 
