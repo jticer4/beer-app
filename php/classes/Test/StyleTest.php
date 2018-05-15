3338f64a6f1aea88c36b6443ff1acaa8e70afa5f
Brent Kie <brent.a.kie@gmail.com>
2018-05-15 11:21:06 -0600

    Created testGetAll, testGetInvalidStyle. Modified testGetValidStyle https://app.asana.com/0/307343870089303/660901941582686

--- php/classes/Test/StyleTest.php
+++ php/classes/Test/StyleTest.php
@@ -88,6 +88,52 @@ class StyleTest extends BeerAppTest {
 	 * test inserting a Style and regrabbing it from mySQL
 	 */
 	public function testGetValidStyleByStyleId() : void {
+		// count the number of rows and save it for later
 		$numRows = $this->getConnection()->getRowCount("style");
+
+		$styleId = null;
+		$style = new Style($styleId, $this->VALID_STYLETYPE);
+		$style->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoStyle = Style:: getStyleByStyleId($this->getPDO(), $style->getStyleId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("style"));
+		$this->assertEquals($pdoStyle->getStyleId(), $styleId);
+		$this->assertEquals($pdoStyle->getStyleType(), $this->VALID_STYLETYPE);
 	}
+
+	/**
+	 * test grabbing a Style that does not exist
+	 */
+	public function testGetInvalidStyleByStyleId() : void {
+		// grab a style id that exceeds the maximum allowable allowable style id
+		$fakeStyleId = null;
+		$style = Style::getStyleByStyleId($this->getPDO(), $fakeStyleId);
+		$this->assertNull($style);
+	}
+
+	/**
+	 * test grabbing all Styles
+	 */
+	public function testGetAllValidStyles(): void {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("style");
+
+		//create new Style and insert into mySQL
+		$styleId = null;
+		$style = new Style($styleId, $this->VALID_STYLETYPE);
+		$style->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Style::getAllStyles($this->getPDO());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("style"));
+		$this->assertCount(1, $results);
+		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\Beer\\Style", $results);
+
+		// grab the result from the array and validate it
+		$pdoStyle = $results[0];
+		$this->assertEquals($pdoStyle->getStyleId(), $styleId);
+		$this->assertEquals($pdoStyle->getStyleType(), $this->VALID_STYLETYPE);
+	}
+
 }
