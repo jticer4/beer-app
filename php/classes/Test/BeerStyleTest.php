df663cc7a5b6289b282a8d53772dea08b1cfb086
Erik Clevenger <eclevenger@cnm.edu>
2018-05-16 15:01:23 -0600

    refactor parent set up
    https://app.asana.com/0/660901941582668/660901941582689

--- php/classes/Test/BeerStyleTest.php
+++ php/classes/Test/BeerStyleTest.php
@@ -45,14 +45,14 @@ class BeerStyleTest extends BeerAppTest {
 		$VALID_ACTIVATION = bin2hex(random_bytes(16));
 
 		//create and insert a Profile up here
-		$this->profile = new \Profile(generateUuid4(), "I make grumpy beer from curmudgeonly back-enders",
+		$profile = new \Profile(generateUuid4(), "I make grumpy beer from curmudgeonly back-enders",
 			$VALID_ACTIVATION, "666 Diablo Rd",
 			"", "Burque", "gKephart@beersnob.com", $VALID_HASH, "https://media.giphy.com/media/3o6Ztqojq5fHrODpjW/giphy.gif", "George K", "NM", "DeepDiveCodingBrewCrew","1", 87101);
-		$this->profile->insert($this->getPDO());
+		$profile->insert($this->getPDO());
 
 
 		//Create and insert Beer from beer style composite
-		$this->beer = new \Beer(generateUuid4(), $this->profile->getProfileId(), "45", 3.2, "Shit Kicker IPA", "Pretty much budlight");
+		$this->beer = new \Beer(generateUuid4(), $profile->getProfileId(), "45", 3.2, "Shit Kicker IPA", "Pretty much budlight");
 		$this->beer->insert($this->getPDO());
 
 		//Create and insert Style from beer style composite
