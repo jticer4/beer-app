d7cb5953f731338e2756d75ea12706fd206efba3
Erik Clevenger <eclevenger@cnm.edu>
2018-05-16 15:21:09 -0600

    Unit Testing change.
    https://app.asana.com/0/660901941582668/660901941582689

--- php/classes/Test/BeerStyleTest.php
+++ php/classes/Test/BeerStyleTest.php
@@ -45,7 +45,7 @@ class BeerStyleTest extends BeerAppTest {
 		$VALID_ACTIVATION = bin2hex(random_bytes(16));
 
 		//create and insert a Profile up here
-		$profile = new \Profile(generateUuid4(), "I make grumpy beer from curmudgeonly back-enders",
+		$profile = new Profile(generateUuid4(), "I make grumpy beer from curmudgeonly back-enders",
 			$VALID_ACTIVATION, "666 Diablo Rd",
 			"", "Burque", "gKephart@beersnob.com", $VALID_HASH, "https://media.giphy.com/media/3o6Ztqojq5fHrODpjW/giphy.gif", "George K", "NM", "DeepDiveCodingBrewCrew","1", 87101);
 		$profile->insert($this->getPDO());
