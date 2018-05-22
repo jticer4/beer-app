53335ba1550cc63e54cd006b97e3e4e636676f95
Brent Kie <brent.a.kie@gmail.com>
2018-05-22 16:07:53 -0600

    Style API has been created with a GET method https://app.asana.com/0/660901941582668/679110671431388

--- public_html/style/index.php
+++ public_html/style/index.php
@@ -1,7 +1,53 @@
 <?php
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config");
+
+use Edu\Cnm\Beer\Style;
+
 /**
- * Created by PhpStorm.
- * User: brent
- * Date: 22-May-18
- * Time: 13:54
- */
+ * API for the Style class
+ * @author <bkie3@cnm.edu>
+ *
+ **/
+
+// verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+// prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	// grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/???.ini");
+
+	// determine which HTTP method was used
+	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
+
+	// sanitize input
+	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+
+	if (($method === "DELETE" || $method === "PUT") && (empty($id) === true)) {
+				throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+	}
+
+	// handle GET request - if id is present that style is returned, otherwise all styles are returned
+	if($method === "GET") {
+		// set XSRF cookie
+		setXsrfCookie();
+
+		// get a specific tweet or all tweets and update reply
+		if(empty($id) === false) {
+					$reply->data = Style::getStyleByStyleId($pdo, $id);
+		} else {
+					$reply->data = Style::getAllStyles($pdo)->toArray();
+		}
+	}
+}
