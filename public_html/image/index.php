a126bb6335ca9fd3ae63f04bc25edd3adc69faf1
Brent Kie <brent.a.kie@gmail.com>
2018-05-23 13:33:50 -0600

    No changes to BeerStyle. Keyboard mistake.. TRY block started for Image API https://app.asana.com/0/660901941582668/679110671431393

--- public_html/image/index.php
+++ public_html/image/index.php
@@ -1,1 +1,47 @@
 <?php
+
+
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
+require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
+
+use Edu\Cnm\Beer\Profile;
+
+/**
+ * Cloudinary API for image upload
+ *
+ * @author brentTheDev | bkie3@cnm.edu
+ * @version 1.01 modeled after Marty Bonacci aka BIG DADDY MUSTACHE
+ * @team Fullstack PHP Cohort 20 | Gang of Four
+ */
+
+
+// start session
+if(session_status() !==PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+// prepare an empty reply
+$reply = new StdClass();
+$status->status = 200;
+$reply->data = null;
+
+try {
+
+			// Grab the mySQL Connection
+			$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/???.ini");
+
+			// determine which HTTP method is being used
+			$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+			$profileId = filter_input(INPUT_GET, "profileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+			$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+			$config = readConfig("/etc/apache2/capstone-mysql/???.ini");
+			$cloudinary = json_decode($config["cloudinary"]);
+			\Cloudinary::config(["cloud_name" => $cloudinary->cloudName, "api_key" => $cloudinary->apiKey, "api_secret" => $cloudinary->apiSecret]);
+
+}
