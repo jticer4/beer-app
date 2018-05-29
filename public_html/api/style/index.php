08abe8ea5e94896ede93ae76fc1df36fad1a4939
Brent Kie <brent.a.kie@gmail.com>
2018-05-29 17:07:03 -0600

    Fixed various bugs in Style API https://app.asana.com/0/307343870089303/660901941582686

--- public_html/api/style/index.php
+++ public_html/api/style/index.php
@@ -3,7 +3,7 @@ require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
-require_once("/etc/apache2/capstone-mysql/encrypted-config");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 use Edu\Cnm\Beer\Style;
 
@@ -25,7 +25,7 @@ $reply->data = null;
 
 try {
 	// grab the mySQL connection
-	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/???.ini");
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/beer.ini");
 
 	// determine which HTTP method was used
 	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
@@ -52,5 +52,5 @@ try {
 
 // encode and return reply to the front end caller
 header("Content-type: application/json");
-echo json_decode($reply);
+echo json_encode($reply);
 
