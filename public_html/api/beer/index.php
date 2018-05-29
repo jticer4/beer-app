7c91dd7b9aab236624e41e47aefa29bbb1addd6d
Erik Clevenger <eclevenger@cnm.edu>
2018-05-29 16:01:00 -0600

    double check pdo
    https://app.asana.com/0/660901941582668/679110671431388/f

--- public_html/api/beer/index.php
+++ public_html/api/beer/index.php
@@ -2,9 +2,11 @@
 
 require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
-require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
+
 
 
 use Edu\Cnm\Beer\{
@@ -30,7 +32,7 @@ $reply->data = null;
 
 try {
 	//grab mySQL connection
-	$pdo = connectToEncryptedMySQL("/etc/apache/capstone-mysql/beer.ini");
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/beer.ini");
 
 	//determine which HTTP method is being used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
