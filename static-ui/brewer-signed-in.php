4ed49a07e8c68883b40d23d4ca0bf09385991994
Brent Kie <brent.a.kie@gmail.com>
2018-06-02 15:36:06 -0600

    Boiler plate for brewer who is signed in. https://app.asana.com/0/660901941582668/list

---
+++ static-ui/brewer-signed-in.php
@@ -0,0 +1,26 @@
+<?php require_once ("head-utils.php");?>
+
+<?php require_once("navbar.php");?>
+
+<section id="" class="bg-white py-5">
+	<div class="container py-3">
+		<div class="row">
+			<div class="col-sm-12 p-3">
+				<h1 class="font-weight-bold">influence</h1>
+			</div>
+			<div class="col-sm-4">
+				<img class="img-fluid mb-4 bw" src="" alt="">
+			</div>
+			<div class="col-sm-8">
+				<p class="lead">Allow me to reintroduce myself. I invented swag. Poppin' bottles, puttin' supermodels in the cab.
+					Yo, I'm makin' short term goals, when the weather folds. Just put away the leathers and put ice on
+					the gold. Chilly with enough bail money to free a big Willy. High stakes, I got more at stake than
+					Philly. Let's stick up the world and split it 50/50. Photo shoot fresh, looking like wealth, 'bout to
+					call the papparazzi on myself. Can't leave rap alone, the game needs me. I'm outchere ballin', I know
+					you see my sneaks. Look I'm on my grind cousin, ain't got time for frontin, sensitive thugs ya'll all need hugs.</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+
