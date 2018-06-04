58db9d8fb4fae654ffc45cd68fb448971eb61d86
James Ticer <jticer4@icloud.com>
2018-06-04 09:50:04 -0600

    added the input fields for the sign up page https://app.asana.com/0/656403034581765/660901941582685

---
+++ static-ui/brewer-sign-up.php
@@ -0,0 +1,46 @@
+<?php require_once ("head-utils.php");?>
+
+<?php require_once("navbar.php");?>
+
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>onTap Sign Up</title>
+	</head>
+	<body>
+		<form class="form-horizontal" role="form">
+			<div class="form-group">
+				<label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+				<div class="col-sm-4">
+					<input class="form-control" id="inputEmail3" placeholder="Email">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="inputUsername3" class="col-sm-2 control-label">Username</label>
+				<div class="col-sm-4">
+					<input class="form-control" id="inputUsername3" placeholder="Username">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+				<div class="col-sm-4">
+					<input class="form-control" id="inputPassword3" placeholder="Password">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="inputConfirmPassword3" class="col-sm-2 control-label">Confirm Password</label>
+				<div class="col-sm-4">
+					<input class="form-control" id="inputConfirmPassword3" placeholder="ConfirmPassword">
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-10">
+					<button type="submit" class="btn btn-success">Sign up</button>
+				</div>
+			</div>
+		</form>
+
+	</body>
+</html>
