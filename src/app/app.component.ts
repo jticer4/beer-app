c449d26beea9e905746ade65d0203ed7c8536e95
Brent Kie <brent.a.kie@gmail.com>
2018-06-06 13:26:50 -0600

    Added SessionService to app.component.ts w/ @carjmarq https://app.asana.com/0/660901941582668/list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import {Component} from "@angular/core";
+import {SessionService} from "./shared/services/session.service";
 
 @Component({
 	selector: "angular-example-app",
 	template: require("./app.component.html")
 })
 
-export class AppComponent {}
+export class AppComponent {
+	constructor(
+		protected sessionService: SessionService
+	){}
+}
