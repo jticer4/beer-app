42264732f56214e94d5739eb4628507e6d718174
Brent Kie <brent.a.kie@gmail.com>
2018-06-06 11:04:51 -0600

    Completed profile service w/ @carmarq https://app.asana.com/0/660901941582668/list

--- src/app/shared/services/profile.service.ts
+++ src/app/shared/services/profile.service.ts
@@ -1,16 +1,37 @@
 import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
+
 import {Observable} from "rxjs/Observable";
+import {Status} from "../classes/status";
 import {Profile} from "../classes/profile";
 
 @Injectable()
 export class ProfileService {
 
-	constructor(protected http: HttpClient) {}
+	constructor(protected http: HttpClient) {
 
-	private profileUrl = "https://jsonplaceholder.typicode.com/users/";
-
-	getAllUsers(): Observable<Profile[]> {
-		return(this.http.get<Profile[]>(this.profileUrl));
 	}
+
+	private profileUrl = "api/profile/";
+
+	//call to the Profile API and get a Profile object by its id
+	getProfile(id: string) :Observable<Profile[]> {
+			return(this.http.get<Profile[]>(this.profileUrl + id));
+	}
+
+	//call to the Profile API and get a Profile object by its activation token
+	getProfileByProfileActivationToken(profileActivationToken: string) :Observable<Profile[]> {
+			return(this.http.get<Profile[]>(this.profileUrl + "?profileActivationToken=" + profileActivationToken ));
+	}
+
+	//call to the Profile API and get a Profile object by its email
+	getProfileByProfileEmail(profileEmail: string) :Observable<Profile[]> {
+			return(this.http.get<Profile[]>(this.profileUrl + "?profileEmail=" + profileEmail));
+	}
+
+	//call to the Profile API and get a Profile object by its username
+	getProfileUsername(profileUsername: string) :Observable<Profile[]> {
+			return(this.http.get<Profile[]>(this.profileUrl + "?profileUsername=" + profileUsername));
+	}
+
 }
