3210000e137d509b8c96f8ebc6d2ed3e57676d49
James Ticer <jticer4@icloud.com>
2018-06-06 11:25:22 -0600

    added the beer class https://app.asana.com/0/656403034581765/660901941582685

---
+++ src/app/shared/classes/beer.ts
@@ -0,0 +1,10 @@
+export class Beer {
+	constructor(
+		public beerId: number,
+		public beerProfileId: number,
+		public beerAbv: number,
+		public beerDescription: string,
+		public beerIbu: number,
+		public beerName: string
+	) {}
+}
