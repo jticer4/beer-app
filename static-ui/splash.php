732516de181ecc0af01c879763b527930af658a6
Carlos Marquez <carl.marq95@gmail.com>
2018-06-06 08:46:20 -0600

    added splash page https://app.asana.com/0/660901941582668/679110671431390

---
+++ static-ui/splash.php
@@ -0,0 +1,40 @@
+<?php require_once ("head-utils.php");?>
+
+<?php require_once("navbar.php");?>
+
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+
+<section id="splash" class="splash">
+	<div class="container">
+
+		<div class="row justify-content-center">
+			<div class="box col-sm-8 mt-3">
+			</div>
+		</div>
+		<div class="row justify-content-center">
+			<div class="col-sm-4 mt-4">
+
+				<select class="form-control">
+					<option>Type</option>
+						</select>
+
+				<select class="form-control mt-2">
+					<option>Abv</option>
+				</select>
+
+				<select class="form-control mt-2">
+					<option>IBU</option>
+				</select>
+
+				<select class="form-control mt-2">
+					<option>Distance</option>
+				</select>
+				<button class="my-4 col-sm-12 justify-content-center" (click)="onClickMe()">Submit</button>
+			</div>
+		</div>
+	</div>
+
+	</section>
+
+
+
