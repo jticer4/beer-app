2631b17b027d55afd63488c64d3ef6cadf94f393
James Ticer <jticer4@icloud.com>
2018-06-06 10:25:45 -0600

    added the auth service https://app.asana.com/0/656403034581765/660901941582685

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,44 @@
+import {JwtHelperService} from "@auth0/angular-jwt";
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+
+
+@Injectable()
+
+export class AuthService {
+
+
+	private token : string = localStorage.getItem("jwt-token");
+
+	constructor(
+		private jwtHelperService: JwtHelperService,
+		private http: HttpClient
+	) {}
+
+	loggedIn() {
+		if (!this.token) {
+			return false;
+		}
+
+		const tokenExpired: boolean = this.jwtHelperService.isTokenExpired(this.token);
+		return !tokenExpired
+	}
+
+
+	public isAuthenticated(): boolean {
+		const token = localStorage.getItem('jwt-token');
+
+		// // Check whether the token is expired and return true or false
+		return !this.jwtHelperService.isTokenExpired(token);
+	}
+
+	decodeJwt() : any {
+		let isLoggedIn : boolean = this.loggedIn();
+
+		if (!isLoggedIn) {
+			return false;
+		}
+
+		return this.jwtHelperService.decodeToken(this.token);
+	}
+}
