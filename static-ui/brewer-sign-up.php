df6d755e7c233e2a70a7bdaa25a51f3ae399fc26
James Ticer <jticer4@icloud.com>
2018-06-07 10:10:46 -0600

    added the sign up component.html https://app.asana.com/0/656403034581765/660901941582685

--- static-ui/brewer-sign-up.php
+++ static-ui/brewer-sign-up.php
@@ -12,27 +12,27 @@
 	<body>
 		<form class="form-horizontal " role="form">
 			<div class="form-group">
-				<label for="inputEmail" class="col-sm-2 control-label">Email</label>
+				<label for="email" class="col-sm-2 control-label">Email</label>
 				<div class="col-sm-4">
-					<input maxlength="128" class="form-control" id="inputEmail" placeholder="Email" ngModel required>
+					<input maxlength="128" class="form-control" id="email" placeholder="Email" ngModel required>
 				</div>
 			</div>
 			<div class="form-group">
-				<label for="inputUsername" class="col-sm-2 control-label">Username</label>
+				<label for="username" class="col-sm-2 control-label">Username</label>
 				<div class="col-sm-4">
-					<input class="form-control" id="inputUsername" placeholder="Username" ngModel required>
+					<input class="form-control" id="username" placeholder="Username" ngModel required>
 				</div>
 			</div>
 			<div class="form-group">
-				<label for="inputPassword" class="col-sm-2 control-label">Password</label>
+				<label for="password" class="col-sm-2 control-label">Password</label>
 				<div class="col-sm-4">
-					<input class="form-control" id="inputPassword" placeholder="Password" ngModel required>
+					<input class="form-control" id="password" placeholder="Password" ngModel required>
 				</div>
 			</div>
 			<div class="form-group">
-				<label for="inputConfirmPassword" class="col-sm-2 control-label">Confirm Password</label>
+				<label for="passwordConfirm" class="col-sm-2 control-label">Confirm Password</label>
 				<div class="col-sm-4">
-					<input class="form-control" id="inputConfirmPassword" placeholder="ConfirmPassword" ngModel required>
+					<input class="form-control" id="passwordConfirm" placeholder="ConfirmPassword" ngModel required>
 				</div>
 			</div>
 			<div class="form-group">
