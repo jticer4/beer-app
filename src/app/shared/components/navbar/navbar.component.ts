8f7949f4d6a3902bf656a103d8208bbd17743291
James Ticer <jticer4@icloud.com>
2018-06-08 10:42:22 -0600

    added the nav bar ts component https://app.asana.com/0/656403034581765/660901941582685

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,37 @@
+import {Component} from "@angular/core";
+import {SignInService} from "../../services/sign.in.service";
+import {Status} from "../../classes/status";
+import {Router} from "@angular/router";
+
+@Component({
+	template: require("./navbar.component.html"),
+	selector: "navbar"
+})
+
+export class NavbarComponent {
+
+	status: Status;
+
+	constructor(
+		private signInService: SignInService,
+		private router: Router
+	) {}
+
+	signOut() : void {
+		this.signInService.getSignOut()
+
+			.subscribe(status => {
+				this.status = status;
+				if(status.status === 200) {
+
+					//delete jwt
+					localStorage.clear();
+
+					//send user back home, refresh page
+					this.router.navigate([""]);
+					location.reload();
+					console.log("goodbye");
+				}
+			});
+	}
+}
