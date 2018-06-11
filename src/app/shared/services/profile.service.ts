3b8307bfa902c37c151ba168bc88a68dcdbe8048
James Ticer <jticer4@icloud.com>
2018-06-11 15:02:30 -0600

    added the location observer and getProfileLocations to the profile constructor https://app.asana.com/0/656403034581765/660901941582685

--- src/app/shared/services/profile.service.ts
+++ src/app/shared/services/profile.service.ts
@@ -4,12 +4,16 @@ import {Injectable} from "@angular/core";
 import {Observable} from "rxjs/Observable";
 import {Status} from "../classes/status";
 import {Profile} from "../classes/profile";
+import {BehaviorSubject} from "rxjs/BehaviorSubject";
+import {Beer} from "../classes/beer";
 
 @Injectable()
 export class ProfileService {
+	protected locationSubject : BehaviorSubject<any> = new BehaviorSubject<any>({});
+	public locationObserver : Observable<any> = this.locationSubject.asObservable();
 
 	constructor(protected http: HttpClient) {
-
+		this.getProfileLocations().subscribe(locations => this.locationSubject.next(locations));
 	}
 
 	private profileUrl = "api/profile/";
@@ -27,6 +31,10 @@ export class ProfileService {
 	//call to the Profile API and get a Profile object by its username
 	getProfileUsername(profileUsername: string) :Observable<Profile[]> {
 			return(this.http.get<Profile[]>(this.profileUrl + "?profileUsername=" + profileUsername));
+	}
+
+	getProfileLocations() :Observable<any> {
+		return(this.http.get<any>(this.profileUrl + "?profileLocation=true"));
 	}
 
 }
